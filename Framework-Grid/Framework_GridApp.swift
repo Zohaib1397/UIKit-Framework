ff8775f44db451d350c041dc3db23de24d2b03b3
Zohaib Ahmed <zohaib.ahmed1397@gmail.com>
2024-07-21 12:56:07 +0500

    Initial Commit

---
+++ Framework-Grid/Framework_GridApp.swift
@@ -0,0 +1,17 @@
+//
+//  Framework_GridApp.swift
+//  Framework-Grid
+//
+//  Created by Zohaib Ahmed on 21/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct Framework_GridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
